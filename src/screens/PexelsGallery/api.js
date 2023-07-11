fbfc2c4fe64068f941dd53705991b5a787a4c0b5
Guillermo Puente <guillermo.ps09@gmail.com>
2023-07-11 16:57:54 -0400

    Added .env

--- src/screens/PexelsGallery/api.js
+++ src/screens/PexelsGallery/api.js
@@ -5,10 +5,11 @@ import { API_URL } from './config';
 export const fetchImagesFromPexels = async () => {
   const data = await fetch(API_URL, {
     headers: {
-      Authorization: Constants.manifest.extra.PEXELS_API_KEY,
+      Authorization: Constants.expoConfig.extra.PEXELS_API_KEY,
     },
   });
 
+  console.log('>>>>>>>>>>>>>>>>data', data);
   const { photos } = await data.json();
   return photos;
 };
