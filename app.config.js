46bc83d8d0802f9015c7d03dff46631885e1182d
Guillermo Puente <guillermo.ps09@gmail.com>
2023-07-11 17:07:07 -0400

    Added update classic

--- app.config.js
+++ app.config.js
@@ -16,7 +16,8 @@ export default {
       backgroundColor: '#ffffff'
     },
     updates: {
-      fallbackToCacheTimeout: 0
+      fallbackToCacheTimeout: 0,
+      useClassicUpdates: true
     },
     assetBundlePatterns: [
       '**/*'
